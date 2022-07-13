99576483cebbd18286d03f61bb5b38650093b289
Mattp15 <Matthewrphillips15@gmail.com>
2022-07-12 22:06:31 -0400

    Added top border for stats, drawing Gooby, only up so far

--- js/app.js
+++ js/app.js
@@ -11,18 +11,19 @@ let rightPressed = false;
 let downPressed = false;
 let leftPressed = false;
 let upPressed = false;
-let lastButtonPressed = 'down';
+let lastButtonPressed = 'up';
 let spaceBarPressed = false;
-let goobyX = 88;
-let goobyY = 8;
+let goobyX = 8;
+let goobyY = 80;
+let currentAnimation = 0;
 let animationCounter = 0;
 const gameObjects = [];
 const maps = [];
 let gameMap = null;
 const tileSet1 = [
+    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -38,7 +39,7 @@ const tileSet1 = [
 const drawMap = level => {
     for(let i = 0; i < level.length; i++){
         for(let j = 0; j < level[i].length; j++){
-            ctx.drawImage(bitMap, level[i][j]*8, 0, 8, 8, i*8, j*8, 8, 8);
+            ctx.drawImage(bitMap, level[i][j]*8, 0, 8, 8, j*8, i*8, 8, 8);
         }
     }
 }
@@ -46,6 +47,33 @@ const drawMap = level => {
 const drawGooby = () => {
     let walk = 2;
     animationCounter++;
+    if(upPressed){
+        goobyY -= walk;
+        if(currentAnimation === 0){
+            ctx.drawImage(gooby, 0, 0, 8, 8, goobyX, goobyY, 8, 8);
+        } else if(currentAnimation === 1){
+            ctx.drawImage(gooby, 8, 0, 8, 8, goobyX, goobyY, 8, 8);
+        }
+        if(animationCounter >= 6){
+            currentAnimation++;
+            animationCounter = 0;
+            if(currentAnimation > 1){
+                currentAnimation = 0;
+            }
+        }
+    } else {
+        switch(lastButtonPressed){
+            case 'up':
+                ctx.drawImage(gooby, 0, 0, 8, 8, goobyX, goobyY, 8, 8);
+                break;
+            case 'down':
+                break;
+            case 'left':
+                break;
+            case 'right':
+                break;
+        }
+    }
 
 }
         //Player movement functions
@@ -62,21 +90,21 @@ const drawGooby = () => {
             } else if(e.keyCode === 83){//down
                 downPressed = true;
                 lastButtonPressed = "down";
-            } else if(e.keyCode === 32){
+            } else if(e.keyCode === 32){//spacebar
                 spaceBarPressed = true;
             }
         }
 
         const keyUpHandler = e => {//key-up (stopped pressing)
-            if(e.keyCode === 37){//left
+            if(e.keyCode === 65){//left
                 leftPressed = false;
-            } else if(e.keyCode === 39){//right
+            } else if(e.keyCode === 68){//right
                 rightPressed = false;
-            } else if(e.keyCode === 38){//up
+            } else if(e.keyCode === 87){//up
                 upPressed = false;         
-            } else if(e.keyCode === 40){//down
+            } else if(e.keyCode === 83){//down
                 downPressed = false;  
-            } else if(e.keyCode === 32){
+            } else if(e.keyCode === 32){//spacebar
                 spaceBarPressed = false;
             }
         }
@@ -87,8 +115,9 @@ const draw = () => {
         ctx.fillStyle = "rgb(20,20,20)";
         ctx.fillRect(0,0,96,96);
         drawMap(tileSet1);
+        drawGooby();
     }),1000/fps;
 }
 draw();
-// document.addEventListener("keydown", keyDownHandler, false);
-// document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
