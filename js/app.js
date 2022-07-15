0302a822a5b68cd6b2a669404999747c2f169839
Mattp15 <Matthewrphillips15@gmail.com>
2022-07-14 23:02:58 -0400

    Made some notes

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ class Enemy extends DefaultObject{
     }
     movementGenerator() {
         setInterval(() => {
-            this.random = Math.floor(Math.random() * 4) +1;
+            this.random = Math.floor(Math.random() * 5) +1;
         }, 500 / this.speed);
     }
 }
@@ -82,7 +82,10 @@ const tileSet1 = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 const objectsTileSet1 = [];
-tempObject = new Enemy(40, 40, 8, 8, 'blueSlime', false, 0.1, 2);
+tempObject = new Enemy(42, 36, 8, 8, 'blueSlime', false, 0.1, 2);
+tempObject.movementGenerator();
+objectsTileSet1.push(tempObject);
+tempObject = new Enemy(18, 24, 8, 8, "blueSlime", false, 0.1, 4);
 tempObject.movementGenerator();
 objectsTileSet1.push(tempObject);
 
@@ -98,6 +101,8 @@ gameMaps.push(bundle);
 let map = gameMaps[0].map;
 gameObjects = gameMaps[0].gameObject;
 
+//consider something to create an agro radius, something like monsterx - goobyx < certain number?
+//consider the logic from before, if obj[i].x > goobyX then obj[i].x - speed. Lets try to get some sort of timing to prevent a hard b-line for player on dumber mobs-possible extension goal
 
 const drawEnemy = obj => {
     for(let i = 0; i < obj.length; i++){
@@ -105,30 +110,34 @@ const drawEnemy = obj => {
         console.log(obj[i].movement);
         obj[i].animationCounter += obj[i].speed;
     if(obj[i].mobType === 'blueSlime'){
+        //do the if/else here for chase
+       //switch in the else{}
         switch(obj[i].random){
         case 1:
-            if(!collision(obj[i].x  - obj[i].speed, obj[i].y, map)){
+            if(!collision(obj[i].x  - obj[i].speed - 2, obj[i].y, map)){
             obj[i].x -= obj[i].speed;
-            obj[i].previousDirection = 'left';
+            obj[i].previousDirection = 'left';//I don't think I need this
             }
             break;
         case 2:
-            if(!collision(obj[i].x, obj[i].y - obj[i].speed, map)){
+            if(!collision(obj[i].x, obj[i].y - obj[i].speed - 4, map)){
             obj[i].y -= obj[i].speed;
             obj[i].previousDirection = 'up';
             }
             break;
         case 3:
-            if(!collision(obj[i].x  + obj[i].speed, obj[i].y, map)){
+            if(!collision(obj[i].x  + obj[i].speed + 4, obj[i].y, map)){
             obj[i].x += obj[i].speed;
             obj[i].previousDirection = 'right';
             }
             break;
          case 4:
-            if(!collision(obj[i].x, obj[i].y + obj[i].speed, map)){
+            if(!collision(obj[i].x, obj[i].y + obj[i].speed + 2, map)){
             obj[i].y += obj[i].speed;
             obj[i].previousDirection = 'down';
             }
+            break;
+        default:
             break;
         }
 
